7599cbb56bd9ad1caddd49ef6fd54a58a5f72686
Kiran Kapoor <kapoorkiran@gmail.com>
2017-09-24 21:19:48 -0500

    encrypted string using web form

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+from caesar import rotate_string
+app = Flask(__name__)
+app.config['DEBUG'] = True
+form="""<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form method="post" action="result"><b><font size=5>Rotate by:</font></b> 
+      <input type=\"text\" name="rot" value=\"0\">
+    <br><textarea type=\"textarea\" name="text"></textarea>
+    <input type=submit value=\"Submit Query\">
+    </body>
+</html>"""
+
+@app.route("/")
+def index():
+    return form
+@app.route("/result", methods=['POST'])
+def encrypt():
+    rot1=int(request.form['rot'])
+    strToBeEncrupted=request.form['text']
+    encrypted=rotate_string(strToBeEncrupted,rot1)
+    return "<h1>"+encrypted+"</h1>"
+app.run()
