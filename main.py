7b144b9118d22ac2c5254a051eb02e1737a6b648
Kiran Kapoor <kapoorkiran@gmail.com>
2017-09-24 21:28:29 -0500

    returning string in same form

--- main.py
+++ main.py
@@ -6,37 +6,37 @@ form="""<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <!-- create your form here -->
       <form method="post" action="result"><b><font size=5>Rotate by:</font></b> 
       <input type=\"text\" name="rot" value=\"0\">
-    <br><textarea type=\"textarea\" name="text"></textarea>
+    <br><textarea type=\"textarea\" name="text">{0}</textarea>
     <input type=submit value=\"Submit Query\">
     </body>
 </html>"""
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 @app.route("/result", methods=['POST'])
 def encrypt():
     rot1=int(request.form['rot'])
     strToBeEncrupted=request.form['text']
     encrypted=rotate_string(strToBeEncrupted,rot1)
-    return "<h1>"+encrypted+"</h1>"
+    return form.format(encrypted)
 app.run()
